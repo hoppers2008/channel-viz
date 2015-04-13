efa41719d62a3f498d408bff08b07b04b422e259
hoppers2008 <ben.hopwood@thealloy.com>
2015-04-13 08:55:38 +0100

    Update follows.js

--- follows.js
+++ follows.js
@@ -15,8 +15,8 @@
 	var defaultKey		= 'cUPqHM0TeqNz8l2qVgWZLn6TMSh7kDDx5C0DWfZ21jX0sziO', // Unique master Xively API key to be used as a default
 		defaultFeeds	= [526320563], // Comma separated array of Xively Feed ID numbers
 		applicationName	= "Who's the nut muncher!?", // Replaces Xively logo in the header
-		dataDuration	= '90days', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
-		dataInterval	= 10800, // Default interval for data to be displayed (in seconds)
+		dataDuration	= '12hours', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
+		dataInterval	= 30, // Default interval for data to be displayed (in seconds)
 		dataColor		= '0A1922', // CSS HEX value of color to represent data (omit leading #)
 		hideForm		= 1; // To hide input form use value of 1, otherwise set to 0
 
