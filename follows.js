ccf5bcb962f17da425df352ef661a631c9313c11
hoppers2008 <ben.hopwood@thealloy.com>
2015-04-13 08:46:48 +0100

    Update follows.js

--- follows.js
+++ follows.js
@@ -12,9 +12,9 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= 'HvfqEJi3vnvfoZnJBOQX2zUnD1puljogEZyZESxdzFass5RD', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [972918848], // Comma separated array of Xively Feed ID numbers
-		applicationName	= 'Alloy Application', // Replaces Xively logo in the header
+	var defaultKey		= 'cUPqHM0TeqNz8l2qVgWZLn6TMSh7kDDx5C0DWfZ21jX0sziO', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [526320563], // Comma separated array of Xively Feed ID numbers
+		applicationName	= "Who's the nut muncher?", // Replaces Xively logo in the header
 		dataDuration	= '90days', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 10800, // Default interval for data to be displayed (in seconds)
 		dataColor		= '0A1922', // CSS HEX value of color to represent data (omit leading #)
